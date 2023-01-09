57e357b5c11dba0b87f0624c014d8932ea57a5a1
Senak9 <joaomarcelo.o.rocha@gmail.com>
2023-01-09 11:29:16 -0300

    "feat: criando header"

--- src/componentes/Header/index.jsx
+++ src/componentes/Header/index.jsx
@@ -1,17 +1,36 @@
 import "./styles.css";
 import Logo from "../../assets/logo.svg";
-import Close from "../../assets/close-icon.svg";
+import Profile from "../../assets/profile.svg";
+import Logout from "../../assets/logout.svg";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    navigate("/");
+  }
   return (
-    <div className="container-header">
-      <header>
+    <header>
+      <div className="width-limit content-header">
         <div className="logo">
           <img src={Logo} alt="logo" />
           <h1>Reading Table</h1>
         </div>
-      </header>
-    </div>
+        <div className="container-sign-out">
+          <div className="profile-area">
+            <img src={Profile} alt="profile" />
+            <strong>Usuario</strong>
+          </div>
+          <img
+            src={Logout}
+            alt="logout"
+            className="sing-out"
+            onClick={handleLogout}
+          />
+        </div>
+      </div>
+    </header>
   );
 }
 
