143e6ae5a02dd7a7addafe48b14fd72f871b5e5d
Senak9 <joaomarcelo.o.rocha@gmail.com>
2023-01-10 12:29:48 -0300

    feat: criando componente RandomItens.

---
+++ src/componentes/RandomItens/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+function RandomItens() {
+  return (
+    <div className="container-random">
+      <div className="random-head blocks-head">fdavdfv</div>
+      <div className="random-body blocks-body">ffaf</div>
+    </div>
+  );
+}
+
+export default RandomItens;
--- src/paginas/Home/index.jsx
+++ src/paginas/Home/index.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import "./styles.css";
 import Header from "../../componentes/Header";
+import RandomItens from "../../componentes/RandomItens";
 
 function Home() {
   return (
     <div className="container-home">
       <Header />
+      <section>
+        <div container-box>
+          <RandomItens />
+          <div>MDJKDKKM</div>
+        </div>
+        <div>ssss</div>
+      </section>
     </div>
   );
 }
