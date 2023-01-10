bac70cde1e4068ae325570f4b8fceedf6988c1cd
Senak9 <joaomarcelo.o.rocha@gmail.com>
2023-01-10 13:22:19 -0300

    feat: atualizando componetes.

--- src/componentes/GroupsItens/index.jsx
+++ src/componentes/GroupsItens/index.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 function GroupsItens() {
   return (
     <div className="container-groups">
-      <div className="groups-head blocks-head">fdavdfv</div>
+      <div className="groups-head blocks-head">Grupos</div>
       <div className="groups-body blocks-body">ffaf</div>
     </div>
   );
--- src/componentes/UpdatedItens/index.jsx
+++ src/componentes/UpdatedItens/index.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 function UpdatedItens() {
   return (
     <div className="container-updated">
-      <div className="updated-head blocks-head">fdavdfv</div>
+      <div className="updated-head blocks-head">Recentes</div>
       <div className="updated-body blocks-body">ffaf</div>
     </div>
   );
